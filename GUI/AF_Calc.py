6d877fa6fe15cda7a3a7efdecfedb0a46181b217
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-09-21 18:05:56 -0600

    close to working af

--- GUI/AF_Calc.py
+++ GUI/AF_Calc.py
@@ -16,17 +16,13 @@ Dependencies:
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits import mplot3d
 #config file contains some useful constants that we'll make use of 
 from config import *
 
 def find_phase_shift(theta_0, phi_0, dx, dy):
     '''
     Calculate the phase shifts required to steer in certain direction
-    
-    Notes
-    -----
-    This program uses the config file which gives parameters for default array
 
     Parameters
     ----------
@@ -45,10 +41,11 @@ def find_phase_shift(theta_0, phi_0, dx, dy):
     '''
     #used Balanis
     # k = 2pi/lam, and dx/y are represented in terms of fractions of a wavelength 
-    beta_X = -2*np.pi*dx*np.sin(np.deg2rad(theta_0))*np.cos(np.deg2rad(phi_0)) 
-    beta_Y = -2*np.pi*dy*np.sin(np.deg2rad(theta_0))*np.sin(np.deg2rad(phi_0))
+     
+    beta_X = -2 * np.pi * dx * np.sin(theta_0)*np.cos(phi_0) 
+    beta_Y = -2 * np.pi * dy * np.sin(theta_0)*np.sin(phi_0)
 
-    return (beta_X, beta_y)
+    return beta_X, beta_Y
 
 def dispAF(dx, dy, beta_x, beta_y):
     '''
@@ -66,7 +63,7 @@ def dispAF(dx, dy, beta_x, beta_y):
     beta_X: float 
         Progressive phase shift in x direction
     beta_Y: float 
-        Progressive phase shift in X direction
+        Progressive phase shift in y direction
 
     Returns
     -------
@@ -75,41 +72,46 @@ def dispAF(dx, dy, beta_x, beta_y):
     '''
    
     #TODO define THETA and PHI meshgrid
-    theta = np.linspace(0, np.pi, 300)
+    theta = np.linspace(0, np.pi/2, 300)
     phi = np.linspace(0, np.pi * 2, 600)
     THETA, PHI = np.meshgrid(theta, phi)
-    Sxm = 1
-    for m in range(0,np.sqrt(NUM_ELEMENTS)+1):
+    Nside = int(np.sqrt(NUM_ELEMENTS))
+    Sxm = 0
+    for m in range(Nside):
         Sxm += np.exp(1j*m*(2*np.pi*dx*np.sin(THETA)*np.cos(PHI) + beta_x)) 
     
-    Syn = 1 
-    for n in range(0, np.sqrt(NUM_ELEMENTS)+1): 
-        Syn += np.exp(1j*n*(2*np.pi*np.sin(THETA)*np.sin(PHI) + beta_y)) 
+    Syn = 0 
+    for n in range(Nside): 
+        Syn += np.exp(1j*n*(2*np.pi*dy*np.sin(THETA)*np.sin(PHI) + beta_y)) 
 
-    AF = Sxm*Syn
-    AF_mag = np.abs(AF)
+    AF = Sxm * Syn
+    AF_mag = np.abs(AF) 
     AF_mag_norm = AF_mag/np.max(AF_mag)
-    AF_mag_norm_dB = 20*np.log10(AF_mag_norm)
     #convert to cartesian coords
-    X = AF_mag_norm_dB * np.sin(THETA) * np.cos(PHI)
-    Y = AF_mag_norm_dB * np.sin(THETA) * np.sin(PHI)
-    Z = AF_mag_norm_dB * np.cos(THETA)
-    plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(X,Y,Z, cmap = 'viridis', edgecolor = 'none')
+    X = AF_mag_norm * np.sin(THETA) * np.cos(PHI)
+    Y = AF_mag_norm * np.sin(THETA) * np.sin(PHI)
+    Z = AF_mag_norm * np.cos(THETA)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    surf = ax.plot_surface(X,Y,Z,cmap ='plasma', 
+    linewidth = 0, antialiased =False) 
+    ax.set_xlim(-1.01, 1.01)
+    ax.set_ylim(-1.01, 1.01)
+    ax.set_zlim(0, 1.01)
+    ax.set_title('Array Factor')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('3D Array Factor')
+    fig.colorbar(surf)
     plt.show()
 
 def main():
-    dx = input('enter the horizontal spacing dx in terms of wavelengths: ')
-    dy = input('enter the horizontal spacing dx in terms of wavelengths: ')
-    desired_phi = input('enter the azimuthal steering direction (degrees): ')
-    desired_theta = input('enter the elevation steering direction (degrees): ')
+    dx = float(input('enter the horizontal spacing dx in terms of wavelengths: '))
+    dy = float(input('enter the vertical spacing dy in terms of wavelengths: '))
+    desired_phi = float(input('enter the azimuthal steering direction (degrees): '))
+    desired_theta = float(input('enter the elevation steering direction (degrees): '))
     beta_x, beta_y = find_phase_shift(desired_theta, desired_phi, dx, dy)
-    AF_Calc(dx,dy,beta_x,beta_y)
+    dispAF(dx,dy,beta_x,beta_y)
 
 
 #run that shit
